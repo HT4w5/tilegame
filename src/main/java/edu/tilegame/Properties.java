23efa233457632ed811d36731621decf36a55318
HT4w5 <1683707746@qq.com>
2025-06-29 01:37:46 +0800

    Tidy files.

--- src/main/java/edu/tilegame/Properties.java
+++ src/main/java/edu/tilegame/Properties.java
@@ -2,5 +2,5 @@ package edu.tilegame;
 
 public class Properties {
     public final static String GAME_TITLE = "Tilegame";
-    public final static String GAME_VERSION = "0.0.1";
+    public final static String GAME_VERSION = "1.0-SNAPSHOT";
 }
